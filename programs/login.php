5fd4b900f165ef090f829ae7528898c577a4b556
h519kami <h520519z@mails.cc.ehime-u.ac.jp>
2022-12-12 11:14:46 +0900

    20221212

--- programs/login.php
+++ programs/login.php
@@ -1,6 +1,8 @@
 <?php
 $d = mysqli_connect("host", "pbl2", "pbl2") or die("接続失敗");
 print("接続成功<BR>");
+mysqli_select_db("user_table", $d);
+mysqli_select_db("SELECT ")
 mysqli_close($d) or die("切断失敗");
 print ("切断成功")  
 ?>
--- programs/regi.php
+++ programs/regi.php
@@ -1,7 +1,7 @@
 <?php
 $d = mysqli_connect("host", "pbl2", "pbl2") or die("接続失敗");
 print("接続成功<BR>");
-mysqli_select_db("user_table", $d);
+
 mysqli_close($d) or die("切断失敗");
 print ("切断成功")  
 ?>
