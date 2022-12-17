759afe660e20f9a0afb6cecf1711e32acbbf78d0
h308naka <h520308x@mails.cc.ehime-u.ac.jp>
2022-12-17 16:50:20 +0900

    曲詳細表示(api）

--- programs/views/detail_song.php
+++ programs/views/detail_song.php
@@ -8,12 +8,20 @@
   <link rel="stylesheet" href="./detail_css/border.css">
   <link rel="stylesheet" href="./detail_css/tab.css">
   <?php
-  require '../methods/db_m.php';
+  //review
+  //require '../methods/db_m.php';
 
-  $db = new GetReview();
+  //$db = new GetReview();
 
-  $res1 = $db->get_all_user_review('john1234');
-  $res2 = $db->get_all_track_review('7iN1s7xHE4ifF5povM6A48');
+  //$res1 = $db->get_all_user_review('john1234');
+  //$res2 = $db->get_all_track_review('7iN1s7xHE4ifF5povM6A48');
+  
+  //detail
+  require '../vendor/autoload.php';
+  require '../methods/api_request.php';
+
+  $result = get_track_info($_GET['spotify_id']);
+  $result1 = get_artist_info($result->tracks[0]->album->artists[0]->id);
 
   ?>
 </head>
@@ -29,21 +37,24 @@
   </nav>
 
   <div class="border" style="text-align: center">
-    <h3>ひまわりの約束</h3>
+    <h3><?= $result->tracks[0]->name?></h3>
     <hr>
 
     <div class="area">
       <input type="radio" name="tab_name" id="tab1" checked>
       <label class="tab_class" for="tab1">タブ1</label>
       <div class="content_class">
-        <p>タブ1のコンテンツを表示します</p>
         <p>
           <?php
-          foreach( $res2 as $e ){
-            echo "評価：".$e['score']."点<br>";
-            echo "コメント<br>";
-            echo $e['comment']."<br>";
-          }
+          echo "アーティスト名：" .$result->tracks[0]->artists[0]->name. "<br>";
+          echo "収録アルバム名：" .$result->tracks[0]->album->name. "<br>";
+          echo "アルバムリリース日：" .$result->tracks[0]->album->release_date. "<br>";
+          echo "ジャンル：" .$result1->genres[0]. "<br>";
+          //foreach( $res2 as $e ){
+            //echo "評価：".$e['score']."点<br>";
+            //echo "コメント<br>";
+            //echo $e['comment']."<br>";
+          //}
           ?>
         </p>
       </div>
