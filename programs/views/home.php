ee4a5a5ac088954f14e899fc0f54e81d414bab3e
h256tana <h520256c@mails.cc.ehime-u.ac.jp>
2023-01-23 12:12:39 +0900

    202301123_new_h256tana

--- programs/views/home.php
+++ programs/views/home.php
@@ -9,6 +9,7 @@
 <link rel="stylesheet" href="./detail_css/tab.css">
 
 <?php
+ session_start();
  require '../methods/api_request.php';
  require '../methods/db_m.php';
  require '../methods/sortByKey.php';
@@ -26,7 +27,7 @@
       <li><a class=”current” href=home.php> Home </a></li>
       <li><a href=search.html> Search </a></li>
       <li><a href=communitie.php> Community </a></li>
-      <li><a href=”#”> Profile </a></li>
+      <li><a href=profiel.php> Profile </a></li>
     </ul>
   </nav>
 
@@ -34,18 +35,19 @@
   ファイル・データ読み込み系コード
   -->
   <?php
+    $_SESSION['comm_id'] = 1;
 
     //DBインスタンス生成
     $db_review = new GetReview();
     //get_score( type, communitie_id ) typeはtrueが曲、falseがユーザー
 
     //曲のIDと平均点取得（ spotify_id, AVE(score) ）
-    $track_score = $db_review->get_score( true, 1 );
+    $track_score = $db_review->get_score( true, $_SESSION['comm_id'] );
     //曲の平均点ランキング取得
     $track_id_rank = sortByKey( 'AVG(score)', SORT_DESC, $track_score );
 
     //ユーザーのIDと平均点取得（ spotify_id, AVE(score) ）
-    $user_score = $db_review->get_score( false, 1 );
+    $user_score = $db_review->get_score( false, $_SESSION['comm_id'] );
     //ユーザーの平均点ランキング取得
     $user_id_rank = sortByKey('AVG(score)', SORT_DESC, $user_score );
 
@@ -58,7 +60,7 @@
 
     //曲情報取得,関連情報一挙表示
     $track_info_test = get_track_info($track_id_rank[0]['spotify_id']);
-    //echo $track_info_test->tracks[0]->album->name;
+    echo $track_info_test->tracks[0]->album->name;
     //echo '<br><br>';
     //var_dump($track_info_test->tracks[0]);
 
@@ -81,13 +83,6 @@
     <h3>楽曲ランキング</h3>
       <div id="track_ranking">
         <?php
-          /*
-          //ソート結果を順番に表示するコード
-          foreach( $sorted_array as $key => $value ){
-            echo $key+1 . '. ' . $value['title'].'<br>';
-          }
-          */
-
           //DBランキング表示領域
 
           //ソート結果を順番に表示するコード
@@ -97,15 +92,27 @@
            $id_tmp = $value['spotify_id'];
            $spotify_url = "https://open.spotify.com/embed/track/"."$id_tmp";
 
+           //ランキング配列内の曲情報取得
+           $track_info = get_track_info($track_id_rank[$key]['spotify_id']);
+
+           //順位表示
            echo $key+1 . '. ';
+
+           //曲詳細画面へのリンク表示
+           echo "<a href=\"./detail_song.php?spotify_id=".$id_tmp."\">"
+           .$track_info->tracks[0]->album->name.
+           "</a>";
+
+           //曲のスコア表示
+           echo ', '. $value['AVG(score)'] . '<br>';
+
            //spotifyの枠で表示
            echo "<iframe src=".$spotify_url."
            width=\"40%\"
            height=\"123\"
            frameborder=\"0\"
            allowtransparency=\"true\"
-           allow=\"encrypted-media\"></iframe>";
-           echo ', '. $value['AVG(score)'] . '<br>';
+           allow=\"encrypted-media\"></iframe><br>";
           }
 
         ?>
